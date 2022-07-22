f9f9ecc77dd486e5c63c6f81ef5c407fe6985585
Senoaji Pamungkas <pamungkas.seno11@example.com>
2022-07-22 14:54:54 +0700

    test

--- cetakLaporan.php
+++ cetakLaporan.php
@@ -11,6 +11,7 @@
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
+    <!-- dsdas -->
     <script>
         $(document).ready(function() {
             $('#example').DataTable();
